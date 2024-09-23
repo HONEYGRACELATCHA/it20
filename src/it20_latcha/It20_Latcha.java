7d1e471fea524e9f9baff5488145f4f37db2c10d
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-09-23 23:13:03 +1030

    LO1_Displaying valuesin the arrays

--- src/it20_latcha/It20_Latcha.java
+++ src/it20_latcha/It20_Latcha.java
@@ -11,5 +11,10 @@ public class It20_Latcha {
 
       String [] name = {"Honey", "Grace", "Shekina"};
     
+      //Display values
+      System.out.print("First Name:" + name[0]);
+      System.out.print("Second Name:" + name[1]);
+      System.out.print("Third Name:" + name[2]);
+
 }
     }
