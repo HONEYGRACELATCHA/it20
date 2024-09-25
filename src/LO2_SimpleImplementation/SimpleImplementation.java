29847bfe22f7754643c4d41688423d69f966d1d8
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-09-25 18:03:19 +1030

    LO2 - 3.1 Simple Implementation

---
+++ src/LO2_SimpleImplementation/SimpleImplementation.java
@@ -0,0 +1,16 @@
+
+package LO2_SimpleImplementation;
+
+
+
+public class SimpleImplementation {
+    
+    public static void main(String[] args) {
+
+         //Declaring arrays
+        String [] name = {"Honey", "Lovely", "Noah", "Noah"};
+    
+         //Displaying arrays
+        System.out.print(name.length);
+ }
+}
