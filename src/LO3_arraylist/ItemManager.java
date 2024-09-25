42ab570c4d7f23c4acd18b71691d3df71b8eec6d
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-09-26 02:05:58 +1030

    Methods to add user's input

--- src/LO3_arraylist/ItemManager.java
+++ src/LO3_arraylist/ItemManager.java
@@ -1,19 +1,28 @@
-
 package LO3_arraylist;
+
 import java.util.ArrayList;
 
-
 public class ItemManager {
-    
+
     private ArrayList<String> items;
-    
+
     // Arraylist constructor
     public ItemManager() {
-        items = new ArrayList<>() ;
+        items = new ArrayList<>();
     }
+
     // Method to display items
-    
     public void displayItems() {
         System.out.println("Items in the list: " + items);
     }
+
+    //Methods to add user's input
+    public void addItems (String item1, String item2, String item3) {
+        items.add(item1);
+        items.add(item2);
+        items.add(item3);
+        System.out.println("You added: " + items);
+  }
+
+
 }
--- src/LO3_arraylist/main.java
+++ src/LO3_arraylist/main.java
@@ -8,14 +8,20 @@ public class main {
 
         Scanner in = new Scanner(System.in);
         ItemManager list = new ItemManager();
-        
+
 //Initialize Variables
         String item1, item2, item3, newitem, finditem;
         int index;
-        
+
         //Displaying items
         list.displayItems();
         
+        //user input
+        System.out.println("Enter 3 items to add: ");
+        item1 = in.nextLine();
+        item2 = in.nextLine();
+        item3 = in.nextLine();
+
 
     }
 }
