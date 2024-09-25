9d09919846db8422168e84e931fb0a345064fed7
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-09-25 18:08:53 +1030

    LO2 - 3.2 Display Via For Loop

---
+++ src/LO2_DisplayViaForLoop/DisplayViaForLoop.java
@@ -0,0 +1,17 @@
+
+package LO2_DisplayViaForLoop;
+
+
+public class DisplayViaForLoop {
+    
+    public static void main(String[] args) {
+
+         //Declaring arrays
+        String [] name = {"Honey", "Lovely", "Ynnah", "Noah"};
+    
+         //Displaying via for loop
+        for(int i = 0; i < name.length; i++){
+            System.out.println(name[3]);
+        }
+ }
+}
