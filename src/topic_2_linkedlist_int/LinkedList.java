24a4c0c31e8fcb197a28926e5cfa91a9618a1968
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-09-27 00:49:00 +1030

    Added switching and deleting nodes from the list

--- src/topic_2_linkedlist_int/LinkedList.java
+++ src/topic_2_linkedlist_int/LinkedList.java
@@ -1,9 +1,9 @@
-package topic_2_linkedList_int;
+package topic_2_linkedlist_int;
 
 public class LinkedList {
 
     private node head;
-    
+
     //Method to add a new node
     public void add(int data) {
         node newNode = new node(data);
@@ -17,6 +17,7 @@ public class LinkedList {
         }
         current.next = newNode;
     }
+
     //Method to print linked list
     public void printList() {
         node current = head;
@@ -25,6 +26,71 @@ public class LinkedList {
             current = current.next;
         }
         System.out.println("null");
+    }
+    //Method to delete a node by value
+
+    public void deleteByValue(int value) {
+        if (head == null) {
+            return;
+        }
+
+        if (head.data == value) {
+            head = head.next;
+            return;
+        }
+
+        node current = head;
+        while (current.next != null) {
+            if (current.next.data == value) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    //Method to move a node to a new position/swap nodes
+    public void moveNodePointer(int currentIndex, int newIndex) {
+        if (head == null || currentIndex == newIndex) {
+            return;
+        }
+
+        node current = head;
+        node prev = null;
+        node movingNode = null;
+        node movingPrev = null;
+
+        for (int i = 0; current != null && i < currentIndex; i++) {
+            movingPrev = prev;
+            prev = current;
+            current = current.next;
+        }
+        movingNode = current;
+
+        if (movingNode == null) {
+            return;
+        }
+
+        if (movingPrev != null) {
+            movingPrev.next = movingNode.next;
+        } else {
+            head = movingNode.next;
+        }
+
+        current = head;
+        prev = null;
+        for (int i = 0; current != null && i < newIndex; i++) {
+            prev = current;
+            current = current.next;
+        }
+
+        if (prev != null) {
+            movingNode.next = current;
+            prev.next = movingNode;
+        } else {
+            movingNode.next = head;
+            head = movingNode;
+        }
     }
 
 }
--- src/topic_2_linkedlist_int/main.java
+++ src/topic_2_linkedlist_int/main.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class main {
 
     public static LinkedList list = new LinkedList();
-    public static Scanner sc = new Scanner(System.in);
+    public static Scanner in = new Scanner(System.in);
 
     //This is a block of methods
     public static void adding() {
         boolean repeat = true;
-          
+
         while (repeat) {
             System.out.println("What Integer do you like to add?");
             System.out.print("Integer: ");
-            int element = sc.nextInt();
+            int element = in.nextInt();
             list.add(element);
             System.out.println("Integer ' " + element + " '  successfully added");
             System.out.println("Would you like to add more?" + "\nPress 1 for YES \nPress 2 for NO");
-            int resp = sc.nextInt();
+            int resp = in.nextInt();
 
             if (resp == 1) {
                 repeat = true;
@@ -26,13 +26,81 @@ public class main {
                 repeat = false;
             } else {
                 System.out.println("Invalid input. Please enter 1 or 2.");
-                sc.close();
+                in.close();
+                break;
+            }
+        }
+    }
+
+    public static void move() {
+        System.out.println("Enter index you want to switch: ");
+        int element1 = in.nextInt();
+        System.out.println("Enter index you want to switch: ");
+        int element2 = in.nextInt();
+
+        list.moveNodePointer(element1, element2);
+        list.printList();
+    }
+
+    public static void display() {
+        list.printList();
+    }
+
+    public static void delete() {
+        System.out.println("What Integer would you like to delete?");
+        list.printList();
+        int element = in.nextInt();
+        list.deleteByValue(element);
+        System.out.println("Integer '" + element + "' successfully deleted!");
+        System.out.print("List: ");
+        list.printList();
+    }
+
+    public static void main(String[] args) {
+
+        //Displaying Menu Choices
+        System.out.println("You're Here at Linkedlist Program!");
+        boolean exit = false;
+        while (!exit) {
+            System.out.println();
+            System.out.println("-----------------------");
+            System.out.println("1. Add Integer");
+            System.out.println("2. Move or Swap Integer");
+            System.out.println("3. Display");
+            System.out.println("4. Delete");
+            System.out.println("5. Exit");
+            System.out.println("-----------------------");
+            System.out.println();
+
+            try {
+                System.out.print("Response: ");
+                int choice = in.nextInt();
+                switch (choice) {
+                    case 1:
+                        adding();
+                        break;
+                    case 2:
+                        move();
+                        break;
+                    case 3:
+                        display();
+                        break;
+                    case 4:
+                        delete();
+                        break;
+                    case 5:
+                        exit = true;
+                        System.out.println("Thank you for using Simple Linkedlist Program. Goodbye!");
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please select a valid option.");
+                }
+            } catch (Exception e) {
+                System.out.println("Error 404. Input Invalid");
                 break;
             }
 
         }
-
     }
-    }
-    
 
+}
