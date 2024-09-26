b93e988077e366dbcd2b6fbc16d381ceb51bd5c9
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-09-26 21:50:16 +1030

    Adding method to print

--- src/topic_2_linkedlist_string/LinkedList.java
+++ src/topic_2_linkedlist_string/LinkedList.java
@@ -17,5 +17,15 @@ public class LinkedList {
         }
         current.next = newNode;
     }
+     
+     //Method to print the linked list
+    public void printlist() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
 
 }
