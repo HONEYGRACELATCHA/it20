6f7e867b53ef4094cbcbaf52615c78959e42ce5b
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-09-27 01:00:47 +1030

    Added switching and deleting nodes from the list

--- src/topic_2_linkedlist_int/LinkedList.java
+++ src/topic_2_linkedlist_int/LinkedList.java
@@ -27,4 +27,69 @@ public class LinkedList {
         }
         System.out.println("null");
     }
+
+    //Method to delete a node by value
+    public void deleteByValue(int value) {
+        if (head == null) {
+            return;
+        }
+
+        if (head.data == value) {
+            head = head.next;
+            return;
+        }
+
+        node current = head;
+        while (current.next != null) {
+            if (current.next.data == value) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    //Method to move a node to a new position/swap nodes
+    public void moveNodePointer(int currentIndex, int newIndex) {
+        if (head == null || currentIndex == newIndex) {
+            return;
+        }
+
+        node current = head;
+        node prev = null;
+        node movingNode = null;
+        node movingPrev = null;
+
+        for (int i = 0; current != null && i < currentIndex; i++) {
+            movingPrev = prev;
+            prev = current;
+            current = current.next;
+        }
+        movingNode = current;
+
+        if (movingNode == null) {
+            return;
+        }
+
+        if (movingPrev != null) {
+            movingPrev.next = movingNode.next;
+        } else {
+            head = movingNode.next;
+        }
+
+        current = head;
+        prev = null;
+        for (int i = 0; current != null && i < newIndex; i++) {
+            prev = current;
+            current = current.next;
+        }
+
+        if (prev != null) {
+            movingNode.next = current;
+            prev.next = movingNode;
+        } else {
+            movingNode.next = head;
+            head = movingNode;
+        }
+    }
 }
