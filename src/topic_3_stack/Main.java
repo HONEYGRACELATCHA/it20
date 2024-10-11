a962e2a3304aca952e59f0e82b00c128ea03302f
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-10-11 23:48:52 +1100

    Added user interface to interact with the stack

--- src/topic_3_stack/Main.java
+++ src/topic_3_stack/Main.java
@@ -7,6 +7,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-    }
+        Stack st = new Stack(5);
 
+        while (true) {
+            System.out.println("------------------");
+            System.out.println("Choose number you want to perform"
+                    + "\n1. Stack size"
+                    + "\n2. Push number"
+                    + "\n3. Peek top Element"
+                    + "\n4. Pop top Element"
+                    + "\n5. Exit");
+            System.out.println("================");
+
+        }
+    }
 }
