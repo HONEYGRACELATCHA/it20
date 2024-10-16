b389a39d5bb36b9d93c422e44082cd20efa43190
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-10-16 18:48:55 +1100

    Added codes in stack.java

--- src/topic_3_stack_calculator/Stack.java
+++ src/topic_3_stack_calculator/Stack.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package topic_3_stack_calculator;
 
-/**
- *
- * @author HOME
- */
 public class Stack {
-    
+
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
+
+    public Stack(int size) {
+        this.maxSize = size;
+        this.stackArray = new int[maxSize];
+        this.top = -1;
+    }
+
 }
