d34ecb217e2b7abaeadf5277b1b0d744cd031595
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-10-16 18:52:36 +1100

    Added extend JFrame

--- src/topic_3_stack_calculator/StackCalculator.java
+++ src/topic_3_stack_calculator/StackCalculator.java
@@ -6,8 +6,34 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Stack;
 
-public class StackCalculator {
+public class StackCalculator extends JFrame implements ActionListener {
 
-  
+    private Stack<Integer> stack;
+    private JTextArea display;
+
+    public StackCalculator() {
+        stack = new Stack<>();
+        setupFrame();
+        setupDisplay();
+        setupButtons();
+    }
+
+    private void setupFrame() {
+        setTitle("Stack Calculator");
+        setSize(300, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout(10, 10));
+        setResizable(false);
+    }
+
+    private void setupDisplay() {
+        display = new JTextArea(3, 20);
+        display.setEditable(false);
+        display.setFont(new Font("Arial", Font.PLAIN, 16));
+        display.setMargin(new Insets(10, 10, 10, 10));
+        JScrollPane scrollPane = new JScrollPane(display);
+        add(scrollPane, BorderLayout.NORTH);
+    }
+    
 
 }
